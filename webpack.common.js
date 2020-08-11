a2d7519f1273292bd36f77c21d751151fff2d4c6
Greg Pfaff <greg.pfaff@silvercar.com>
2020-08-10 21:53:33 -0500

    fix webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,11 @@
-const webpack = require('webpack');
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+const{ CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+  },
   module: {
     rules: [
       {
@@ -26,24 +29,15 @@ module.exports = {
   resolve: {
     extensions: ['*', '.js', '.jsx']
   },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new TerserPlugin()
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
   ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-    minimize: true,
-    minimizer: [new TerserPlugin()],
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, './dist'),
   },
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  }
 };
