6b4c2cbec1ece95ce24f581ddc60bdc7516b83b4
Greg Pfaff <greg.pfaff@silvercar.com>
2020-09-17 06:42:39 -0500

    update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -56,13 +56,27 @@ module.exports = {
     }),
   ],
   optimization: {
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+    optimization: {
+      namedModules: true,
+      minimize: true,
+      splitChunks: {
+        chunks: 'async',
+        minSize: 20000,
+        maxSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        automaticNameDelimiter: '~',
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
         },
       },
     },
